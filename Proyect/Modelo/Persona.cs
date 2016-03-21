b922ed0366df2f22068a1ff248574ef5844eee5c
jairdz04 <jairdz04@gmail.com>
2016-03-21 11:17:50 -0500

    Clases1

---
+++ Proyect/Modelo/Persona.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyect.Modelo
+{
+    public class Persona
+    {
+#region "Atributos"
+        public string id;
+        public string TipoID;
+        public string identificacion;
+        public string Nombres;
+        public string Apellidos;
+        public string Sexo;
+        public string F_Nacimiento;
+
+#endregion
+
+        #region "propiedades"
+        public string ID {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string tipoId {
+            get { return TipoID; }
+            set { TipoID = value; }
+        
+        }
+
+        public string identi {
+            get { return identificacion; }
+            set { identificacion = value; }
+                          }
+
+        public string nombres {
+            get { return Nombres; }
+            set { Nombres = value; }
+                               }
+
+        public string apellidos {
+            get { return Apellidos; }
+            set { Apellidos = value; }
+                                }
+
+        public string sexo {
+            get { return Apellidos; }
+            set { Sexo = value; }
+                           }
+
+        public string f_naci {
+            get { return F_Nacimiento; }
+            set { F_Nacimiento = value; }
+                            }
+
+        #endregion
+
+        #region "constructores"
+
+        public Persona() {
+            this.id = "";
+            this.TipoID = "";
+            this.identificacion = "";
+            this.Nombres = "";
+            this.Apellidos = "";
+            this.Sexo = "";
+            this.F_Nacimiento = "";
+        }
+
+        public Persona(string id, string TipoID, string identificacion, string Nombres, string Apellidos, string Sexo, string F_Nacimiento) {
+
+            this.id = id;
+            this.TipoID = TipoID;
+            this.identificacion = identificacion;
+            this.Nombres = Nombres;
+            this.Apellidos = Apellidos;
+            this.Sexo = Sexo;
+            this.F_Nacimiento = F_Nacimiento;
+        
+        }
+
+        #endregion
+
+        #region "metodos SobreEscritos"
+
+        public override string ToString()
+        {
+            return "Id : " + this.id +
+                "\nTipo Identificacion : " + this.TipoID +
+                "\nIdentificacion :" + this.identificacion +
+                "\nNombres : " + this.Nombres +
+                "\nApellidos :" + this.Apellidos +
+                "\nSexo : " + this.Sexo +
+                "\nFecha de Nacimiento :" + this.F_Nacimiento ;
+
+        }
+
+        public override int GetHashCode() {
+            return this.ToString().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Persona o = (Persona)obj;
+            bool result = false;
+
+            if ((this.id == o.id) &&
+                (this.TipoID == o.TipoID) &&
+                (this.identificacion == o.identificacion) &&
+                (this.Nombres == o.Nombres) &&
+                (this.Apellidos == o.Apellidos) &&
+                (this.Sexo == o.Sexo) &&
+                (this.F_Nacimiento == o.F_Nacimiento))
+
+                result = true;
+
+            return result;
+        }
+
+
+
+        #endregion
+
+
+
+
+    }
+}
